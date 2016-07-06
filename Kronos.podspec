6713265d00924a5206f5a63910b71c646f0a9699
Martin Conte Mac Donell <martin@lyft.com>
2016-07-06 15:05:06 -0700

    Fix comment formatting

--- Kronos.podspec
+++ Kronos.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'Kronos'
-  s.version = '0.0.3'
+  s.version = '0.0.4'
   s.license = 'Apache License, Version 2.0'
   s.summary = 'Elegant NTP client in Swift'
   s.homepage = 'https://github.com/lyft/Kronos'
--- Sources/NSTimer+ClosureKit.swift
+++ Sources/NSTimer+ClosureKit.swift
@@ -42,10 +42,10 @@ final class BlockTimer: NSObject {
     /**
     Creates and returns a block-based NSTimer object and schedules it on the current run loop.
 
-    - parameter: interval  The number of seconds between firings of the timer.
-    - parameter: repeated  If true, the timer will repeatedly reschedule itself until invalidated. If false,
-                           the timer will be invalidated after it fires.
-    - parameter: handler   The closure that the NSTimer fires.
+    - parameter interval: The number of seconds between firings of the timer.
+    - parameter repeated: If true, the timer will repeatedly reschedule itself until invalidated. If false,
+                          the timer will be invalidated after it fires.
+    - parameter handler:  The closure that the NSTimer fires.
 
     - returns: a new NSTimer object, configured according to the specified parameters.
     */
