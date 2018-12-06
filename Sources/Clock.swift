4e4d4105059227bcb608cbf1518e21cdc5a38b92
Ryan Rhee <ryan.y.rhee@gmail.com>
2018-12-06 17:07:00 -0500

    oops - 'date' instead of 'data'

--- Sources/Clock.swift
+++ Sources/Clock.swift
@@ -42,7 +42,7 @@ public struct Clock {
 
     /// The most accurate date that we have so far (nil if no synchronization was done yet)
     public static var now: Date? {
-        return self.annotatedlNow?.data
+        return self.annotatedlNow?.date
     }
 
     /// Same as `now` except with analytic metadata about the time
