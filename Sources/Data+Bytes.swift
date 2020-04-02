c39d296fd4d84ca10f19a3661a87885c2a948717
Andrew Podkovyrin <podkovyrin@gmail.com>
2020-04-02 10:53:32 +0300

    Fix "Initialization of 'UnsafeBufferPointer<T>' results in a dangling buffer pointer" warnings

--- Sources/Data+Bytes.swift
+++ Sources/Data+Bytes.swift
@@ -70,7 +70,7 @@ extension Data {
     /// - parameter data: The byte to be appended.
     mutating func append(byte data: Int8) {
         var data = data
-        self.append(UnsafeBufferPointer(start: &data, count: 1))
+        self.append(Data(bytes: &data, count: MemoryLayout<Int8>.size))
     }
 
     /// Appends the given unsigned integer (32 bits; 4 bytes) into the receiver Data.
@@ -78,7 +78,7 @@ extension Data {
     /// - parameter data: The unsigned integer to be appended.
     mutating func append(unsignedInteger data: UInt32, bigEndian: Bool = true) {
         var data = bigEndian ? data.bigEndian : data.littleEndian
-        self.append(UnsafeBufferPointer(start: &data, count: 1))
+        self.append(Data(bytes: &data, count: MemoryLayout<UInt32>.size))
     }
 
     /// Appends the given unsigned long (64 bits; 8 bytes) into the receiver Data.
@@ -86,6 +86,6 @@ extension Data {
     /// - parameter data: The unsigned long to be appended.
     mutating func append(unsignedLong data: UInt64, bigEndian: Bool = true) {
         var data = bigEndian ? data.bigEndian : data.littleEndian
-        self.append(UnsafeBufferPointer(start: &data, count: 1))
+        self.append(Data(bytes: &data, count: MemoryLayout<UInt64>.size))
     }
 }
